208b50aecbddd6d449c46c461bfcb555e648e927
Marek Snopkowski <snpy@snpy.info>
2013-11-18 20:19:04 +0100

    Replace exit statement with RuntimeException

--- exceptional/Exception/Http404Error.php
+++ exceptional/Exception/Http404Error.php
@@ -9,8 +9,7 @@ class Http404Error extends Exception
     public function __construct()
     {
         if (!isset($_SERVER['HTTP_HOST'])) {
-            echo 'Run PHP on a server to use \OBV\Exceptional\Exception\Http404Error.' . PHP_EOL;
-            exit(0);
+            throw new \RuntimeException('Run PHP on a server to use \OBV\Exceptional\Exception\Http404Error.');
         }
         parent::__construct($_SERVER['REQUEST_URI'] . ' can\'t be found.');
     }
