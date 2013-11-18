beb3052039a9c578db47231d5107d91916be31fd
Marek Snopkowski <snpy@snpy.info>
2013-11-18 22:44:48 +0100

    Add encoder class
    It will handle encoding and decoding of postponed exceptional report data.

---
+++ exceptional/Encoder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OBV\Exceptional;
+
+/**
+ * @file
+ * @since  18.11.13 22:41 GMT+2
+ * @author GG Team <gg@team.tld>
+ */
+
+/**
+ * Class Encoder
+ *
+ * @package OBV\Exceptional
+ */
+class Encoder
+{
+    /**
+     * @param string $url
+     * @param string $compressedData
+     *
+     * @return string
+     */
+    public static function encode($url, $compressedData)
+    {
+        return base64_encode($url . "\x00" . $compressedData);
+    }
+
+    /**
+     * @param string $data
+     *
+     * @return array Array with $url and $compressedData values.
+     */
+    public static function decode($data)
+    {
+        return explode("\x00", base64_decode($data), 2);
+    }
+}
