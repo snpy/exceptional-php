3ec2c2144e42b98ab2af8359d6bf57612dc0f286
Marek Snopkowski <snpy@snpy.info>
2013-11-19 16:56:45 +0100

    Update simple example

--- examples/simple.php
+++ examples/simple.php
@@ -1,7 +1,7 @@
 <?php
 
 // setup Exceptional with the following two lines
-\OBV\Exceptional\Exceptional::setup("YOUR-API-KEY");
+\OBV\Exceptional\Exceptional::setup('YOUR-API-KEY');
 
 // control which errors are caught with error_reporting
 error_reporting(E_ALL);
